a6dbd169cfcf796141f12772270daf1bfeae60a0
Pavel <pasha051205@gmail.com>
2024-10-07 21:02:00 +0300

    v1.3 (Pre-release) [v2.0]
    Disabling locomotion animation for crouching and prone at will has been added (not tested yet).
    Changed project folder names and some file names, and also fixed a small bug with the avatar body (2 double tops were detected), as well as added sharp tops for Boots and tinted it a bit.
    The “Authors” menu was removed (who needs excessive advertising? If it's already in the avatar description :D), as well as Thumbnail.png with ThumbnailOverride script for the sake of project memory optimization.

---
+++ vrcfox unity project (B&C)/Assets/scripts/SceneLabel.cs
@@ -0,0 +1,30 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public class SceneLabel
+{
+	private static GUIStyle style= new GUIStyle();
+
+	static SceneLabel()
+	{
+		style.normal.textColor = Color.yellow;
+		style.fontSize = 50;
+		style.fontStyle = FontStyle.Bold;
+		style.alignment = TextAnchor.MiddleCenter;
+		SceneView.duringSceneGui += OnScene;
+	}
+
+	private static void OnScene(SceneView sceneview)
+	{
+		Handles.BeginGUI();
+		GUILayout.BeginArea(new Rect(0, 0, sceneview.camera.pixelWidth, 100));
+		GUILayout.Label(SceneManager.GetActiveScene().name, style);
+		GUILayout.EndArea();
+
+		Handles.EndGUI();
+	}
+}
+#endif
