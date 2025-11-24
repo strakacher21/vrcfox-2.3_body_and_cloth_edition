8d37b808da170643322827df7285c7f2f7ad9a5e
Pavel <pavel@tarahovich.ru>
2025-11-25 00:14:07 +0300

    working prototype (very rough, but it works)

---
+++ vrcfox garry's mod project (B&C)/output/strakacher21/lua/autorun/vrcfox_pm.lua
@@ -0,0 +1,6 @@
+AddCSLuaFile()
+
+player_manager.AddValidModel( "vrcfox_bnc", "models/vrcfox (B&C)/vrcfox (B&C).mdl" )
+list.Set( "PlayerOptionsModel", "vrcfox_bnc", "models/vrcfox (B&C)/vrcfox (B&C).mdl" )
+
+player_manager.AddValidHands( "vrcfox_bnc", "models/weapons/c_arms_cstrike.mdl", 0, "10000000" )
